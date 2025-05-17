84425f1fb1738db6744904ad1706df027c1d829e
XavierM25 <xaviermg2504@gmail.com>
2025-05-17 17:43:04 -0500

    Adds Clerk integration and support for Spanish localization (esMX) in Astro configuration. Updated dependencies in package.json and package-lock.json to include @astrojs/node, @clerk/astro and @clerk/localizations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import clerk from "@clerk/astro";
+import { esMX } from "@clerk/localizations";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    clerk({
+      localization: esMX,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: node({
+    mode: "standalone",
+  }),
+  output: "server",
 });
