11e5706ce00c37e4bfffe1ef642210cc0402d129
XavierM25 <xaviermg2504@gmail.com>
2025-05-18 19:32:42 -0500

    Removes Clerk integration and related dependencies in package.json and package-lock.json. Updated Astro configuration in astro.config.mjs and removed middleware.ts file. Added login form on the main page and improved font import in global.css.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import clerk from "@clerk/astro";
-import { esMX } from "@clerk/localizations";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    clerk({
-      localization: esMX,
-    }),
-  ],
   vite: {
     plugins: [tailwindcss()],
   },
